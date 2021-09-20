0e4d5db5e1d6591a568deaf109384ed980b874af
esh4rak <eshrak.sea4@gmail.com>
2021-09-20 23:51:00 +0600

    mvvm architecture

---
+++ Event Calendar/app/src/main/java/com/example/eventcalendar/views/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.eventcalendar.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import com.example.eventcalendar.R;
+import com.example.eventcalendar.databinding.ActivityMainBinding;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        init();
+    }
+
+
+    private void init() {
+    }
+
+
+
+}
